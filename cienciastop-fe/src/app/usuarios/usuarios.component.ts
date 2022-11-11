a201177e047357f79a6c62aed280e12e1089c2f5
David Alvarado <davidalv@ciencias.unam.mx>
2022-11-11 12:05:15 -0600

    búsqueda de usuarios terminada (?) attempting to corregir la parte de búsqueda de productos

--- cienciastop-fe/src/app/usuarios/usuarios.component.ts
+++ cienciastop-fe/src/app/usuarios/usuarios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { USUARIOS } from './usuarios.json';
 import { Usuario } from './usuarios';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-usuarios',
@@ -11,10 +12,44 @@ export class UsuariosComponent implements OnInit {
 
   usuarios: Usuario[];
 
+  searchInput: string = "";
+  placeholder: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
     this.usuarios = USUARIOS;
   }
 
+  getMatches(): Usuario[] {
+    console.log(this.searchInput);
+    var matches = []
+    
+    this.usuarios.forEach(e => {
+      if((`${e.nombre} ${e.apellidoPaterno} ${e.apellidoMaterno}`).toLowerCase().includes(this.searchInput.toLowerCase())) {
+        matches.push(e);
+      }
+    })
+
+    return matches;
+  }
+
+  onSubmit(): void {
+    this.usuarios = USUARIOS;
+    document.getElementById("res-bus-usuario").style.display = "none";
+    var matches = this.getMatches();
+    console.log(matches)
+    console.log(this.searchInput);
+    if(matches.length == 0) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `No hubo resultados para la búsqueda "${this.searchInput}"`,
+      });
+    } else {
+      this.placeholder = this.searchInput;
+      document.getElementById("res-bus-usuario").style.display = "block";
+      this.usuarios = matches;
+    }
+  }
 }
--- cienciastop-fe/src/app/usuarios/usuarios.json.ts
+++ cienciastop-fe/src/app/usuarios/usuarios.json.ts
@@ -2,10 +2,10 @@ import { Usuario } from "./usuarios";
 
 export const USUARIOS: Usuario[] = [
     {id: 0, nombre: 'Arca', apellidoPaterno: 'Ghersi', apellidoMaterno: 'Rodriguez', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
-    {id: 1, nombre: 'Arca', apellidoPaterno: 'Ghersi', apellidoMaterno: 'Rodriguez', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
-    {id: 2, nombre: 'Arca', apellidoPaterno: 'Ghersi', apellidoMaterno: 'Rodriguez', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
-    {id: 3, nombre: 'Arca', apellidoPaterno: 'Ghersi', apellidoMaterno: 'Rodriguez', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
-    {id: 4, nombre: 'Arca', apellidoPaterno: 'Ghersi', apellidoMaterno: 'Rodriguez', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
-    {id: 5, nombre: 'Arca', apellidoPaterno: 'Ghersi', apellidoMaterno: 'Rodriguez', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
-    {id: 6, nombre: 'Arca', apellidoPaterno: 'Ghersi', apellidoMaterno: 'Rodriguez', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
+    {id: 1, nombre: 'Charli', apellidoPaterno: 'X', apellidoMaterno: 'CX', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
+    {id: 2, nombre: 'Lana', apellidoPaterno: 'Del', apellidoMaterno: 'Rey', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
+    {id: 3, nombre: 'Ethel', apellidoPaterno: 'Hayden', apellidoMaterno: 'Cain', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
+    {id: 4, nombre: 'FKA', apellidoPaterno: 'Thalia', apellidoMaterno: 'Twigs', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
+    {id: 5, nombre: 'Lorde', apellidoPaterno: 'Yellich', apellidoMaterno: 'O\'Connor', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
+    {id: 6, nombre: 'zorra', apellidoPaterno: 'Ghersi', apellidoMaterno: 'Rodriguez', correo: 'mutante@@@@@@@@@@', numeroCel: 12334444, carrera: 'Putologia', contraseña: '&&&&&&&&&', rol: 'activa', esActivo: true, pumapuntos: 666},
 ]
