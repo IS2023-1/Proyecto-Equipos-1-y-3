c8496d9f43a515d992c354729eb3c141ec480e2f
Antonio S. Dromundo <sebastiandromundo@outlook.com>
2022-11-16 01:28:20 -0600

    Actualizado backend a ultima version

--- ciencias-top-be/src/main/java/com/cienciasTop/CienciasTopApplication.java
+++ ciencias-top-be/src/main/java/com/cienciasTop/CienciasTopApplication.java
@@ -1,4 +1,4 @@
-package is20231.cienciastopbe;
+package com.cienciasTop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ ciencias-top-be/src/main/java/com/cienciasTop/models/entity/Proveedor.java
@@ -0,0 +1,45 @@
+package com.cienciasTop.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "proveedores")
+public class Proveedor implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne
+	@JoinColumn(name = "id_proveedor")
+	private Usuario proveedor;
+	
+	// --------- Getters y Setters -----------
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Usuario getProveedor() {
+		return proveedor;
+	}
+
+	public void setProveedor(Usuario proveedor) {
+		this.proveedor = proveedor;
+	}
+
+}
