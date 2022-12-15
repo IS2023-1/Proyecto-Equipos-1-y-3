bad248534d90f43320d06e4cd301056eba4aa4f4
Joshua Montaño <joshuasmp_2001@ciencias.unam.mx>
2022-12-15 01:03:42 -0600

    vamos aver si esta es la buena

--- ciencias-top-be/src/main/java/com/cienciasTop/models/entity/Rentar.java
+++ ciencias-top-be/src/main/java/com/cienciasTop/models/entity/Rentar.java
@@ -1,15 +1,19 @@
 package com.cienciasTop.models.entity;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 public class Rentar implements Serializable {
@@ -20,13 +24,18 @@ public class Rentar implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne
-	@JoinColumn(name = "id_usuario")
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "id_usuario", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	private Usuario usuario;
 
-	@ManyToOne
-	@JoinColumn(name = "id_producto")
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
+	@JoinColumn(name = "id_producto", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	private Producto producto;
+	
+	@Column(name = "fecha_de_renta", columnDefinition = "TIMESTAMP")
+	private LocalDateTime fecha_de_renta; 
 
 	public Long getId() {
 		return id;
@@ -51,5 +60,12 @@ public class Rentar implements Serializable {
 	public void setProducto(Producto producto) {
 		this.producto = producto;
 	}
+	
+	public LocalDateTime getFecha_de_renta() {
+		return fecha_de_renta;
+	}
 
+	public void setFecha_de_renta(LocalDateTime fecha_de_renta) {
+		this.fecha_de_renta = fecha_de_renta;
+	}
 }
