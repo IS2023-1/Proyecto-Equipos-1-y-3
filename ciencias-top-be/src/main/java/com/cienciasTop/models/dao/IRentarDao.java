e22a46c8dac1dc5f3ed291271234a019db938e48
Raul_EMartinez <raulmartinez@ciencias.unam.mx>
2022-12-15 00:51:23 -0600

    Agregando archivos

--- ciencias-top-be/src/main/java/com/cienciasTop/models/dao/IRentarDao.java
+++
@@ -1,9 +0,0 @@
-package com.cienciasTop.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cienciasTop.models.entity.Rentar;
-
-public interface IRentarDao extends CrudRepository<Rentar,Long>{
-
-}
